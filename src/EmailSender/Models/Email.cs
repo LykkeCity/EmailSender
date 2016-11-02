9b5c80c3d342aafc662da03d1ace9681ce5c8a2a
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-11-02 12:58:39 +0400

    Add email entity.

---
+++ src/EmailSender/Models/Email.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailSender.Models
+{
+    public class Email
+    {
+        public List<string> To { get; set; }
+        public string Subject { get; set; }
+        public bool IsHtml { get; set; }
+        public string Body { get; set; }
+        public List<Attachment> Attachments { get; set; }
+    }
+
+    public class Attachment
+    {
+        public string FileName { get; set; }
+        public string MimeType { get; set; }
+        public string Base64Value { get; set; }
+    }
+}
