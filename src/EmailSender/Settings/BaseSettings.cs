6f3f631f9f6960403e610ac306ffd115611d0f43
Tengiz-Merabishvili <tengiz.merabishvili@gmail.com>
2016-11-02 12:59:39 +0400

    Add ServiceBus settings.

--- src/EmailSender/Settings/BaseSettings.cs
+++ src/EmailSender/Settings/BaseSettings.cs
@@ -4,17 +4,27 @@
     {
         AzureSettings Azure { get; set; }
         string TemplatesLink { get; set; }
+        ServiceBusSettings ServiceBus { get; set; }
     }
 
     public class BaseSettings : IBaseSettings
     {
         public AzureSettings Azure { get; set; }
         public string TemplatesLink { get; set; }
+        public ServiceBusSettings ServiceBus { get; set; }
     }
 
     public class AzureSettings
     {
-        public string LogsConnString { get; set; }
         public string EmailsQueueConnString { get; set; }
+    }
+
+    public class ServiceBusSettings
+    {
+        public string NamespaceUrl { get; set; }
+        public string KeyName { get; set; }
+        public string KeyValue { get; set; }
+        public string Entity { get; set; }
+        public int Port { get; set; }
     }
 }
